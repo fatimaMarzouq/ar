2c115ba798983e12831a09a8621872d93b570055
fatima <fatima@softylus.com>
2021-06-17 13:47:28 +0300

    'migrate'

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,9 @@ from pathlib import Path
 # import dj_database_url
 
 from environs import Env # new
-
+# import django_heroku
+#
+# django_heroku.settings(locals())
 env = Env() # new
 env.read_env() # new
 
